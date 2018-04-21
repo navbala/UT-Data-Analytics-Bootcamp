9e262896da60df8024f7983d01000550713870d2
unknown <nav.balachandran@gmail.com>
2018-04-21 15:19:27 -0500

    added sleep timer after intial browser visit

--- Homework/Web Scraping and Document Databases/scrape_mars.py
+++ Homework/Web Scraping and Document Databases/scrape_mars.py
@@ -31,6 +31,8 @@ def scrape():
     url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     browser.visit(url)
 
+    time.sleep(.5)
+
     # Create BeautifulSoup object; parse with 'html.parser'
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
